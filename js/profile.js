14aede275757be4e3e2616fd5e2a1e398d286cc1
Daniel Gómez <danitusa5@gmail.com>
2018-10-14 06:05:45 +0200

    version 3

---
+++ js/profile.js
@@ -0,0 +1,51 @@
+/* var profile = "<ul><li>moderfuckeeer</li></ul>"; */
+
+let profile = `<div class="contenido"><div class="profile">
+    <div id="profilepic"><div id="profilecenter"><img class="profilepic" src="/img/profile.jpg" alt=""></div></div>
+    </div>
+    <div class="fixspace"></div>
+    <div class="contenido container">
+
+    <h2 class="center">Daniel Gomez</h2>
+    <p class="center">Creativo, UI Designer, Front End developer</p>
+    <p class="justify">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quos itaque unde sequi, minus fuga doloremque ab? Nihil commodi tempora error, accusamus eveniet voluptates ea dolores repudiandae sapiente repellat optio?
+    </p>
+    <h3>Detalles</h3>
+    <ul>
+        <li><strong>Nombre: </strong>Daniel Gomez</li>
+        <li><strong>Edad: </strong>24 Años</li>
+        <li><strong>Locación: </strong>Plasencia, Extremadura, España</li>
+    </ul>
+    <h3>Skills</h3>
+    <p>Cita de alguien a quien admiro</p>
+    <ul>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+    </ul>
+    <h3>Idiomas</h3>
+    <ul>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+    </ul>
+    <h3>Herramientas</h3>
+    <ul>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+        <li>nombre de skill</li>
+    </ul>
+
+
+
+    </div></div>`;
