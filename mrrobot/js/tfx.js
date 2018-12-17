7639837a8ade7d6f3113e501ad8cf55770074b13
Daniel Gómez <danitusa5@gmail.com>
2018-12-17 20:27:34 +0100

    tet mrrobot 2
    add preloader and toogle for l33t

--- mrrobot/js/tfx.js
+++ mrrobot/js/tfx.js
@@ -1,26 +1,29 @@
-
-
 var title = $('#title').text();
 var subtitle = $('#subtitle').text();
 var formation = $('li').html();
 
-setInterval(() => {
-    titleFX();
-    subtitleFX();
-    liTextFx();
-}, 1000);
+//==================================================
+//              Preloader
+//==================================================
 
-setInterval(() => {
-    cTitleFX();
-    csubTitleFX();
-    cliTextFx();
-}, 2400);
+
+$('.preloader img').mouseup(function () {
+
+    $('.preloader img').css('border',' white 12px solid');
+
+    setTimeout(() => {
+        $('.preloader').addClass('preloaderA');
+    }, 1200);
+
+});
+
+
 
 //==================================================
 //              Title Fx
 //==================================================
 function titleFX() {
-    
+
     title = title.replace(/a/g, "4");
     title = title.replace(/e/g, "3");
     title = title.replace(/i/g, "1");
@@ -43,7 +46,7 @@ function cTitleFX() {
 //==================================================
 
 function subtitleFX() {
-    
+
     subtitle = subtitle.replace(/a/g, "4");
     subtitle = subtitle.replace(/e/g, "3");
     subtitle = subtitle.replace(/i/g, "1");
@@ -59,15 +62,15 @@ function csubTitleFX() {
     subtitle = subtitle.replace(/0/g, "o");
     subtitle = subtitle.replace(/X/g, "u");
     $('#subtitle').text(subtitle);
-} 
+}
 
 //==================================================
 //             List Fx
 //==================================================
 
-var list = document.getElementsByTagName('li');  
+var list = document.getElementsByTagName('li');
 
-function liTextFx(){
+function liTextFx() {
     for (let i = 0; i < list.length; i++) {
         let ddd = list[i];
         ddd = $(ddd).text().replace(/a/g, "4");
@@ -76,10 +79,10 @@ function liTextFx(){
         ddd = ddd.replace(/o/g, "0");
         ddd = ddd.replace(/u/g, "x");
         $(list[i]).text(ddd);
-    } 
+    }
 }
 
-function cliTextFx(){
+function cliTextFx() {
     for (let i = 0; i < list.length; i++) {
         let ddd = list[i];
         ddd = $(ddd).text().replace(/4/g, "a");
@@ -88,38 +91,77 @@ function cliTextFx(){
         ddd = ddd.replace(/0/g, "o");
         ddd = ddd.replace(/X/g, "u");
         $(list[i]).text(ddd);
-    } 
+    }
 }
 
 //==================================================
 //              open & close menu
 //==================================================
 
-    $('.menu-foot').mouseup(function () { 
-        $('.menu').addClass('move-menu');
-    });
-    
-    $('.menu-close').mouseup(function () { 
-        $('.menu').removeClass('move-menu');
-    });
+$('.menu-foot').mouseup(function () {
+    $('.menu').addClass('move-menu');
+});
 
-    $('#formation').mouseup(function(){
-        $('.modal').addClass('modal-appear');
-        $('.logo-foot').css('display','none');
-        $('.menu-foot').css('display','none');
-        $('.wrapper').css('justifyContent','center');
-        $('.back').css('opacity','1');
-    });
+$('.menu-close').mouseup(function () {
+    $('.menu').removeClass('move-menu');
+});
 
-    $('.back').mouseup(function(){
-        $('.modal').removeClass('modal-appear');
-        $('.logo-foot').css('display','inherit');
-        $('.menu-foot').css('display','inherit');
-        $('.wrapper').css('justifyContent','space-between');
-        $('.back').css('opacity','0');
-    });
+$('#formation').mouseup(function () {
+    $('.modal').addClass('modal-appear');
+    $('.logo-foot').css('display', 'none');
+    $('.menu-foot').css('display', 'none');
+    $('.wrapper').css('justifyContent', 'center');
+    $('.back').css('opacity', '1');
+});
 
-    
+$('.back').mouseup(function () {
+    $('.modal').removeClass('modal-appear');
+    $('.logo-foot').css('display', 'inherit');
+    $('.menu-foot').css('display', 'inherit');
+    $('.wrapper').css('justifyContent', 'space-between');
+    $('.back').css('opacity', '0');
+});
+
+
 /* function close(){
     
 } */
+
+
+//==================================================
+//              toogle the l33t
+//==================================================
+
+var leetCounter = 0;
+
+function l33t() {
+    if (leetCounter === 0) {
+        leetCounter += 1;
+        titleFX();
+        subtitleFX();
+        liTextFx();
+    } else {
+        leetCounter -= 1;
+        cTitleFX();
+        csubTitleFX();
+        cliTextFx();
+    }
+
+}
+
+$('.logo-foot').mouseup(function () {
+    l33t()
+});
+
+/* setInterval(() => {
+    titleFX();
+    subtitleFX();
+    liTextFx();
+}, 1000);
+
+setInterval(() => {
+    cTitleFX();
+    csubTitleFX();
+    cliTextFx();
+}, 2400);
+ */
